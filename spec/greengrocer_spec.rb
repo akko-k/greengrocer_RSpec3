8ceba2ff6be65ec982bb11b81cee1e8c49c69f62
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-12-23 23:02:57 +0900

    【単体テスト2正常系】修正3

--- spec/greengrocer_spec.rb
+++ spec/greengrocer_spec.rb
@@ -1,33 +1,27 @@
 RSpec.describe Greengrocer do
-    # ▼単体テスト2 正常系（Greengrocerクラスのインスタンス生成）
-    describe ".initialize" do
-      context "インスタンスが生成されたとき" do
-        let(:product_params) { [{ name: "トマト", price: 100 },{ name: "きゅうり", price: 200 }] }
+  # ▼単体テスト2 正常系（Greengrocerクラスのインスタンス生成）
+  describe ".initialize" do
+    context "インスタンスが生成されたとき" do
+      let(:product_params) do
+        [
+          { name: "トマト", price: 100 },
+          { name: "きゅうり", price: 200 }
+        ]
+      end
+      let(:greengrocer) { Greengrocer.new(product_params) }
+      let(:products) { greengrocer.products }
 
-        it "@productsの数とproduct_paramsの要素の数が同じであること" do
-          greengrocer = Greengrocer.new(product_params)
-          expect(greengrocer.products.size).to eq product_params.size
-        end
+      it "@productsの数とproduct_paramsの要素の数が同じであること" do
+        expect(products.size).to eq product_params.size
+      end
 
-        it "@productsとproduct_paramsの1つ目の要素の名前が同じであること" do
-          greengrocer = Greengrocer.new(product_params)
-          expect(greengrocer.products[0].name).to eq product_params[0][:name]
-        end
-
-        it "@productsとproduct_paramsの1つ目の要素の金額が同じであること" do
-          greengrocer = Greengrocer.new(product_params)
-          expect(greengrocer.products[0].price).to eq product_params[0][:price]
-        end
-
-        it "@productsとproduct_paramsの2つ目の要素の名前が同じであること" do
-          greengrocer = Greengrocer.new(product_params)
-          expect(greengrocer.products[1].name).to eq product_params[1][:name]
-        end
-
-        it "@productsとproduct_paramsの2つ目の要素の金額が同じであること" do
-          greengrocer = Greengrocer.new(product_params)
-          expect(greengrocer.products[1].price).to eq product_params[1][:price]
-        end
+      it "@productsとproduct_paramsの1つ目の要素の名前が同じであること" do
+        expect(products[0].name).to eq product_params[0][:name]
+      end
+      
+      it "@productsとproduct_paramsの1つ目の要素の金額が同じであること" do
+        expect(products[0].price).to eq product_params[0][:price]
       end
     end
   end
+end
