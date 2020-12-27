a5a907132b161930672f3880fa247967f707e7ed
kawakami <barcarolle.op60.horowitz@gmail.com>
2020-12-28 00:05:26 +0900

    【単体テスト３正常系】修正2

--- spec/greengrocer_spec.rb
+++ spec/greengrocer_spec.rb
@@ -54,7 +54,24 @@ RSpec.describe Greengrocer do
   # ▼単体テスト4 正常系(disp_productsメソッド)　※戻り値がnil
   describe ".disp_products" do
     context "メソッドが実行されたとき" do
+      let(:product_params) do
+        [
+          { name: "トマト", price: 100 },
+          { name: "きゅうり", price: 200 }
+        ]
+      end
+      let(:greengrocer) { Greengrocer.new(product_params) }
+      let(:products) { greengrocer.products }
       
+      it "productsの1つ目の要素の名前の値が取得できていること" do
+        greengrocer.disp_products
+        expect(products[0].name).to eq "トマト"
+      end
+
+      it "productsの1つ目の要素の金額の値が取得できていること" do
+        greengrocer.disp_products
+        expect(products[0].price).to eq 100
+      end
     end
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,4 +1,4 @@
 RSpec.describe Product do
-    # ▼単体テスト3 正常系（Userクラスのインスタンス生成）
+    # 不要 ▼単体テスト 正常系（Userクラスのインスタンス生成）
     
 end
