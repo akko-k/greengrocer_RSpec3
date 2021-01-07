cb4ad812d87b7855c3cf4d043c610de1e9877bf6
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-01-07 22:13:46 +0900

    【単体テスト5異常系】

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,41 +4,34 @@ RSpec.describe User do
   # ▼単体テスト5 異常系(choose_productメソッド)※入力ミスに対応できているかどうかを確認（大きい整数，0，負の整数，文字列）
   # ▼単体テスト5 正常系(choose_productメソッド)
   describe ".choose_product" do
-    context "商品を選ぶとき" do
-      let(:user) { User.new }
-      let(:product_params) do
-        [
-          { name: "トマト", price: 100},
-          { name: "きゅうり", price: 200}
-        ]
-      end
-      let(:greengrocer) {Greengrocer.new(product_params)}
-      let(:products) { greengrocer.products }
-      
+    let(:product_params) do
+      [
+        { name: "トマト", price: 100 },
+        { name: "きゅうり", price: 200 }
+      ]
+    end
+    let(:greengrocer) { Greengrocer.new(product_params) }
+    let(:products) { greengrocer.products }
 
-      it "入力値が大きい整数の場合（かつ入力値のidが存在しない場合）、再入力を促すこと" do
-        # user.stub(:gets) { 100 }
-        # expect { user.choose_product(products) }
-        #   .to output( "#{products.first.id}から#{products.last.id}の番号から選んでください。").to_stdout
-        #   user.choose_product(products) if $0 == __FILE__
+    context "大きい整数を入力したとき" do
+      it '再入力を促すこと' do
+        select_product_num = StringIO.new("100\n")
+        pronpt_re_enter_msg = "#{products.first.id}から#{products.last.id}の番号から選んでください。"
+        allow(ARGF).to receive(:gets) { select_product_num.gets }
+        expect { User.new.choose_product(products) }
+          .to output(pronpt_re_enter_msg).to_stdout
       end
-
-      it "入力値が0の場合（かつ入力値のidが存在しない場合）、再入力を促すこと" do
+    end
+    context "0を入力したとき" do
+      it '再入力を促すこと' do
       end
-
-      it "入力値が負の整数の場合（かつ入力値のidが存在しない場合）、再入力を促すこと" do
+    end
+    context "負の整数を入力したとき" do
+      it '再入力を促すこと' do
       end
-
-      it "入力値が文字列の場合、再入力を促すこと" do
-      end
-
-      it "入力値のidがが存在する場合、@chosen_productの@idが一致すること" do
-      end
-
-      it "入力値のidがが存在する場合、@chosen_productの@nameが一致すること" do
-      end
-
-      it "入力値のidがが存在する場合、@chosen_productの@priceが一致すること" do
+    end
+    context "文字列を入力したとき" do
+      it '再入力を促すこと' do
       end
     end
   end
