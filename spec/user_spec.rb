542a1ca5d3f6c3b4ad16a635babdd4638224455c
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-01-10 00:04:03 +0900

    【単体テスト5異常系】修正1

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -13,25 +13,18 @@ RSpec.describe User do
     let(:greengrocer) { Greengrocer.new(product_params) }
     let(:products) { greengrocer.products }
 
-    context "大きい整数を入力したとき" do
+    context "idとイコールではない大きい整数，0,負の整数，文字列を入力したとき" do
       it '再入力を促すこと' do
-        select_product_num = StringIO.new("100\n")
-        pronpt_re_enter_msg = "#{products.first.id}から#{products.last.id}の番号から選んでください。"
-        allow(ARGF).to receive(:gets) { select_product_num.gets }
-        expect { User.new.choose_product(products) }
-          .to output(pronpt_re_enter_msg).to_stdout
-      end
-    end
-    context "0を入力したとき" do
-      it '再入力を促すこと' do
-      end
-    end
-    context "負の整数を入力したとき" do
-      it '再入力を促すこと' do
-      end
-    end
-    context "文字列を入力したとき" do
-      it '再入力を促すこと' do
+        id = Product.class_variable_get('@@count')
+        big_positive_integer = 100
+        negative_integer = -1
+        if id != big_positive_integer && id != negative_integer = -1 && id != 0
+          select_product_num = StringIO.new("#{big_positive_integer}\n0\n#{negative_integer}\n文字列\n")
+          pronpt_re_enter_msg = "#{products.first.id}から#{products.last.id}の番号から選んでください。"
+          allow(ARGF).to receive(:gets.to_i) { select_product_num.gets.to_i }
+          expect { User.new.choose_product(products) }
+            .to output(pronpt_re_enter_msg).to_stdout
+        end
       end
     end
   end
