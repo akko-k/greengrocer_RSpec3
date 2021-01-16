6cb4fc7ae7fb12a8654d1dc8f8cac37efcb7e8e8
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-01-16 23:39:38 +0900

    【単体テスト5正常系】

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,8 +1,6 @@
 RSpec.describe User do
   # 不要 ▼単体テスト 正常系（Userクラスのインスタンス生成）
 
-  # ▼単体テスト5 異常系(choose_productメソッド)※入力ミスに対応できているかどうかを確認（大きい整数，0，負の整数，文字列）
-  # ▼単体テスト5 正常系(choose_productメソッド)
   describe ".choose_product" do
     let(:product_params) do
       [
@@ -12,19 +10,34 @@ RSpec.describe User do
     end
     let(:greengrocer) { Greengrocer.new(product_params) }
     let(:products) { greengrocer.products }
+    let(:user) { User.new }
 
-    context "idとイコールではない大きい整数を入力したとき" do
-      it '再入力を促すこと' do
-        id = Product.class_variable_get('@@count')
-        input = "100\n"
-        pronpt_re_enter_msg = "#{products.first.id}から#{products.last.id}の番号から選んでください。"
-        allow(ARGF).to receive(:gets) { input }
-        expect { User.new.choose_product(products) }
-          .to output(pronpt_re_enter_msg).to_stdout
-        allow(ARGF).to receive(:exit) { input }
-        # expect { User.new.choose_product(products) }
-        #   .to 
+    # ▼単体テスト5 正常系(choose_productメソッド)
+    context "存在するidを入力したとき" do
+      let(:correct_input) { "#{products.first.id}\n" }
+      it '@chosen_productとproduct_paramsの名前と同じであること' do
+        allow(ARGF).to receive(:gets){ correct_input }
+        user.choose_product(products)
+        expect(user.chosen_product.name).to eq product_params[0][:name]
       end
+
+      it '@chosen_productとproduct_paramsの金額が同じであること' do
+        allow(ARGF).to receive(:gets){ correct_input }
+        user.choose_product(products)
+        expect(user.chosen_product.price).to eq product_params[0][:price]
+      end
+    end
+
+    # ▼単体テスト5 異常系(choose_productメソッド)※入力ミスに対応できているかどうかを確認（大きい整数，0，負の整数，文字列）
+    context "idとして存在しない大きい整数を入力したとき" do
+      # it '再入力を促すこと' do
+      #   wrong_input = "100\n"
+      #   correct_input = "#{products.first.id}\n"
+      #   allow(ARGF).to receive(:gets).and_return wrong_input, correct_input
+      #   pronpt_re_enter_msg = "#{products.first.id}から#{products.last.id}の番号から選んでください。"
+      #   expect { User.new.choose_product(products) }
+      #     .to output(pronpt_re_enter_msg,).to_stdout
+      # end
     end
 
     context "idとイコールではない0を入力したとき" do
