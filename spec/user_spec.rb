e2fdb8f64c59e2ddea904affd5b02f9c2917df90
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-01-17 20:53:21 +0900

    【単体テスト5異常系】修正8

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -11,32 +11,31 @@ RSpec.describe User do
     let(:greengrocer) { Greengrocer.new(product_params) }
     let(:products) { greengrocer.products }
     let(:user) { User.new }
-
+    let(:correct_input) { "#{products.first.id}\n" }
     # ▼単体テスト5 正常系(choose_productメソッド)
     context "存在するidを入力したとき" do
-      let(:correct_input) { "#{products.first.id}\n" }
+      
       it '@chosen_productとproduct_paramsの名前と同じであること' do
-        allow(ARGF).to receive(:gets){ correct_input }
+        allow(ARGF).to receive(:gets).and_return correct_input
         user.choose_product(products)
         expect(user.chosen_product.name).to eq product_params[0][:name]
       end
 
       it '@chosen_productとproduct_paramsの金額が同じであること' do
-        allow(ARGF).to receive(:gets){ correct_input }
+        allow(ARGF).to receive(:gets).and_return correct_input
         user.choose_product(products)
         expect(user.chosen_product.price).to eq product_params[0][:price]
       end
     end
 
     # ▼単体テスト5 異常系(choose_productメソッド)※入力ミスに対応できているかどうかを確認（大きい整数，0，負の整数，文字列）
+    let(:pronpt_re_enter_msg ) { "#{products.first.id}から#{products.last.id}の番号から選んでください。" }
     context "idとして存在しない大きい整数を入力したとき" do
+      let(:wrong_input) { "100\n" }
       it '再入力を促すこと' do
-        wrong_input = "100\n"
-        correct_input = "#{products.first.id}\n"
         allow(ARGF).to receive(:gets).and_return wrong_input, correct_input
         pronpt_choose_id_msg = "商品の番号を選択 >"
-        pronpt_re_enter_msg = "#{products.first.id}から#{products.last.id}の番号から選んでください。"
-        expect { User.new.choose_product(products) }
+        expect { user.choose_product(products) }
           .to output(pronpt_choose_id_msg + pronpt_re_enter_msg + "\n" + pronpt_choose_id_msg ).to_stdout
       end
     end
