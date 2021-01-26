b2faf4f8fb31e407b51e7f3e5a765cf551674b68
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-01-26 10:07:20 +0900

    【単体テスト1正常系】修正1

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,27 +2,27 @@ RSpec.describe Product do
   # ▼単体テスト1 正常系（Productクラスのインスタンス生成）
   describe ".initialize" do
     context "インスタンスが生成されたとき" do
-      let(:params) { { name: "トマト", price: 100 } }
+      let(:product_params) { { name: "トマト", price: 100 } }
 
       it "@@countが1増加すること" do
-        expect { Product.new(params) }
+        expect { Product.new(product_params) }
           .to change { Product.class_variable_get('@@count') }.by(1)
       end
 
       it "idが正しく振られること" do
         count = Product.class_variable_get('@@count')
-        product = Product.new(params)
+        product = Product.new(product_params)
         expect(product.id).to eq count + 1
       end 
 
       it "名前の値が正しいこと" do
-        product = Product.new(params)
-        expect(product.name).to eq params[:name]
+        product = Product.new(product_params)
+        expect(product.name).to eq product_params[:name]
       end
       
       it "金額の値が正しいこと" do
-        product = Product.new(params)
-        expect(product.price).to eq params[:price]
+        product = Product.new(product_params)
+        expect(product.price).to eq product_params[:price]
       end
     end
   end
