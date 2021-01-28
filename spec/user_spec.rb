f2a739418f2127f29b2919edd2eb67daff8e0977
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-01-28 23:22:30 +0900

    【単体テスト5異常系】修正17 shared_exampleを追加※エラー

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe User do
   # 不要 ▼単体テスト 正常系（Userクラスのインスタンス生成）
 
   describe ".choose_product" do
+    
     let(:product_params) do
       [
         { name: "トマト", price: 100 },
@@ -13,6 +14,10 @@ RSpec.describe User do
     let(:user) { User.new }
     let(:correct_input) { "#{products.first.id}\n" }
     let(:pronpt_re_enter_msg) { /#{products.first.id}から#{products.last.id}の番号から選んでください。/ }
+    # shared_exampleをここで使うか要確認
+    shared_examples '再入力を促すこと' do
+      it { expect { user.choose_product(products) }.to output(pronpt_re_enter_msg).to_stdout }
+    end
     # ▼単体テスト5 正常系(choose_productメソッド)
     context "存在するidを入力したとき" do
       
@@ -32,11 +37,8 @@ RSpec.describe User do
     # ▼単体テスト5 異常系(choose_productメソッド)※不正な値の入力に対応できているかどうかを確認
     context "商品一覧の最初のidより１小さい数の文字列を入力したとき" do
       let(:small_wrong_input) { "#{products.first.id - 1}\n" }
-
-      it '再入力を促すこと' do
-        allow(ARGF).to receive(:gets).and_return small_wrong_input, correct_input
-        expect { user.choose_product(products) }.to output(pronpt_re_enter_msg).to_stdout
-      end
+      allow(ARGF).to receive(:gets).and_return small_wrong_input, correct_input
+      it_behaves_like '再入力を促すこと'
     end
 
     context "商品一覧の最後のidより１大きい数の文字列を入力したとき" do
