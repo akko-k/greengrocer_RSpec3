028746e94cf225dd8f1f277a57b9ea9166d18a55
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-02-03 12:01:03 +0900

    【単体テスト8正常系】修正1

--- spec/greengrocer_spec.rb
+++ spec/greengrocer_spec.rb
@@ -105,7 +105,7 @@ describe ".ask_quantity" do
     end
   end
 end
-# 単体テスト8 正常系】calculate_chargesメソッドが期待通り動作する
+# 【単体テスト8 正常系】calculate_chargesメソッドが期待通り動作する
 #  --> 条件分岐が複数あるので，quantity_of_productが4個のときと5個のときだけでなく，chosen_productも2種類ぐらい確認しておいた方が無難。
 describe ".calculate_charges" do
   let(:product_params) do
@@ -115,70 +115,137 @@ describe ".calculate_charges" do
     ]
   end
   let(:greengrocer) { Greengrocer.new(product_params) }
-  let(:products) { greengrocer.products }
+  let(:products) { greengrocer.products } # 略しているだけ
   let(:user) { User.new }
-  let(:chosen_product) { user.chosen_product }
-  let(:quantity_of_product) { user.quantity_of_product }
+  let(:chosen_product) { products.find{|product| product.id == 53 } }
+  let(:quantity_of_product) { 4 }
   let(:thank_msg) { "お買い上げありがとうございました！" }
 
   context "quantity_of_productが4個，chosen_productが最初の要素のとき" do
-    let(:quantity_of_product_input) { 4 }
-    let(:select_product_num_input) { "#{products.first.id}\n" }
     let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product).floor }円です。" }
     it "正しい合計金額を含む，期待した表示がされること" do
-      allow(ARGF).to receive(:gets).and_return select_product_num_input
-      user.choose_product(products)
-      allow(ARGF).to receive(:gets).and_return quantity_of_product_input
-      user.decide_quantity
-      greengrocer.calculate_charges(user)
       expect{ greengrocer.calculate_charges(user) }.to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
     end
   end
 
-  context "quantity_of_productが4個，chosen_productが最後の要素のとき" do
-    let(:quantity_of_product_input) { 4 }
-    let(:select_product_num_input) { "#{products.last.id}\n" }
-    let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product).floor }円です。" }
-    it "正しい合計金額を含む，期待した表示がされること" do
-      allow(ARGF).to receive(:gets).and_return select_product_num_input
-      user.choose_product(products)
-      allow(ARGF).to receive(:gets).and_return quantity_of_product_input
-      user.decide_quantity
-      greengrocer.calculate_charges(user)
-      expect{ greengrocer.calculate_charges(user) }.to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
-    end
-  end
+  # context "quantity_of_productが4個，chosen_productが最後の要素のとき" do
+  #   let(:quantity_of_product_input) { 4 }
+  #   let(:select_product_num_input) { "#{products.last.id}\n" }
+  #   let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product).floor }円です。" }
+  #   it "正しい合計金額を含む，期待した表示がされること" do
+  #     allow(ARGF).to receive(:gets).and_return select_product_num_input
+  #     user.choose_product(products)
+  #     allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+  #     user.decide_quantity
+  #     greengrocer.calculate_charges(user)
+  #     expect{ greengrocer.calculate_charges(user) }.to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
+  #   end
+  # end
 
-  context "quantity_of_productが5個，chosen_productが最初の要素のとき" do
-    let(:quantity_of_product_input) { 5 }
-    let(:select_product_num_input) { "#{products.first.id}\n" }
-    let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product * 0.9).floor }円です。" }
-    let(:discount_msg) { "5個以上なので10％割引となります！" }
-    it "正しい合計金額を含む，期待した表示がされること" do
-      allow(ARGF).to receive(:gets).and_return select_product_num_input
-      user.choose_product(products)
-      allow(ARGF).to receive(:gets).and_return quantity_of_product_input
-      user.decide_quantity
-      greengrocer.calculate_charges(user)
-      expect{ greengrocer.calculate_charges(user) }.to output("#{discount_msg}\n#{total_price_msg}\n#{thank_msg}\n").to_stdout
-    end
-  end
+  # context "quantity_of_productが5個，chosen_productが最初の要素のとき" do
+  #   let(:quantity_of_product_input) { 5 }
+  #   let(:select_product_num_input) { "#{products.first.id}\n" }
+  #   let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product * 0.9).floor }円です。" }
+  #   let(:discount_msg) { "5個以上なので10％割引となります！" }
+  #   it "正しい合計金額を含む，期待した表示がされること" do
+  #     allow(ARGF).to receive(:gets).and_return select_product_num_input
+  #     user.choose_product(products)
+  #     allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+  #     user.decide_quantity
+  #     greengrocer.calculate_charges(user)
+  #     expect{ greengrocer.calculate_charges(user) }.to output("#{discount_msg}\n#{total_price_msg}\n#{thank_msg}\n").to_stdout
+  #   end
+  # end
 
-  context "quantity_of_productが5個，chosen_productが最後の要素のとき" do
-    let(:quantity_of_product_input) { 5 }
-    let(:select_product_num_input) { "#{products.last.id}\n" }
-    let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product * 0.9).floor }円です。" }
-    let(:discount_msg) { "5個以上なので10％割引となります！" }
-    it "正しい合計金額を含む，期待した表示がされること" do
-      allow(ARGF).to receive(:gets).and_return select_product_num_input
-      user.choose_product(products)
-      allow(ARGF).to receive(:gets).and_return quantity_of_product_input
-      user.decide_quantity
-      greengrocer.calculate_charges(user)
-      expect{ greengrocer.calculate_charges(user) }.to output("#{discount_msg}\n#{total_price_msg}\n#{thank_msg}\n").to_stdout
-    end
-  end
+  # context "quantity_of_productが5個，chosen_productが最後の要素のとき" do
+  #   let(:quantity_of_product_input) { 5 }
+  #   let(:select_product_num_input) { "#{products.last.id}\n" }
+  #   let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product * 0.9).floor }円です。" }
+  #   let(:discount_msg) { "5個以上なので10％割引となります！" }
+  #   it "正しい合計金額を含む，期待した表示がされること" do
+  #     allow(ARGF).to receive(:gets).and_return select_product_num_input
+  #     user.choose_product(products)
+  #     allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+  #     user.decide_quantity
+  #     greengrocer.calculate_charges(user)
+  #     expect{ greengrocer.calculate_charges(user) }.to output("#{discount_msg}\n#{total_price_msg}\n#{thank_msg}\n").to_stdout
+  #   end
+  # end
 end
+
+# # 総合テストになっている
+# describe ".grenngrocer_program" do #テスト名は仮
+#   let(:product_params) do
+#     [
+#       { name: "トマト", price: 100 },
+#       { name: "きゅうり", price: 200 }
+#     ]
+#   end
+#   let(:greengrocer) { Greengrocer.new(product_params) }
+#   let(:products) { greengrocer.products }
+#   let(:user) { User.new }
+#   let(:chosen_product) { user.chosen_product }
+#   let(:quantity_of_product) { user.quantity_of_product }
+#   let(:thank_msg) { "お買い上げありがとうございました！" }
+
+#   context "quantity_of_productが4個，chosen_productが最初の要素のとき" do
+#     let(:quantity_of_product_input) { 4 }
+#     let(:select_product_num_input) { "#{products.first.id}\n" }
+#     let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product).floor }円です。" }
+#     it "正しい合計金額を含む，期待した表示がされること" do
+#       allow(ARGF).to receive(:gets).and_return select_product_num_input
+#       user.choose_product(products)
+#       allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+#       user.decide_quantity
+#       greengrocer.calculate_charges(user)
+#       expect{ greengrocer.calculate_charges(user) }.to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
+#     end
+#   end
+
+#   context "quantity_of_productが4個，chosen_productが最後の要素のとき" do
+#     let(:quantity_of_product_input) { 4 }
+#     let(:select_product_num_input) { "#{products.last.id}\n" }
+#     let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product).floor }円です。" }
+#     it "正しい合計金額を含む，期待した表示がされること" do
+#       allow(ARGF).to receive(:gets).and_return select_product_num_input
+#       user.choose_product(products)
+#       allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+#       user.decide_quantity
+#       greengrocer.calculate_charges(user)
+#       expect{ greengrocer.calculate_charges(user) }.to output("#{total_price_msg}\n#{thank_msg}\n").to_stdout
+#     end
+#   end
+
+#   context "quantity_of_productが5個，chosen_productが最初の要素のとき" do
+#     let(:quantity_of_product_input) { 5 }
+#     let(:select_product_num_input) { "#{products.first.id}\n" }
+#     let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product * 0.9).floor }円です。" }
+#     let(:discount_msg) { "5個以上なので10％割引となります！" }
+#     it "正しい合計金額を含む，期待した表示がされること" do
+#       allow(ARGF).to receive(:gets).and_return select_product_num_input
+#       user.choose_product(products)
+#       allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+#       user.decide_quantity
+#       greengrocer.calculate_charges(user)
+#       expect{ greengrocer.calculate_charges(user) }.to output("#{discount_msg}\n#{total_price_msg}\n#{thank_msg}\n").to_stdout
+#     end
+#   end
+
+#   context "quantity_of_productが5個，chosen_productが最後の要素のとき" do
+#     let(:quantity_of_product_input) { 5 }
+#     let(:select_product_num_input) { "#{products.last.id}\n" }
+#     let(:total_price_msg){ "合計金額は#{(chosen_product.price * quantity_of_product * 0.9).floor }円です。" }
+#     let(:discount_msg) { "5個以上なので10％割引となります！" }
+#     it "正しい合計金額を含む，期待した表示がされること" do
+#       allow(ARGF).to receive(:gets).and_return select_product_num_input
+#       user.choose_product(products)
+#       allow(ARGF).to receive(:gets).and_return quantity_of_product_input
+#       user.decide_quantity
+#       greengrocer.calculate_charges(user)
+#       expect{ greengrocer.calculate_charges(user) }.to output("#{discount_msg}\n#{total_price_msg}\n#{thank_msg}\n").to_stdout
+#     end
+#   end
+# end
 
 # quantity_of_productが4個のとき,
 # chosen_productがトマトのとき
