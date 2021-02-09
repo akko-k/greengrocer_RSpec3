48bfdef66b3a5bba63af8574ca4a87e9541273fa
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-02-09 12:49:29 +0900

    【単体テスト6正常系】修正3

--- spec/greengrocer_spec.rb
+++ spec/greengrocer_spec.rb
@@ -1,15 +1,14 @@
 RSpec.describe Greengrocer do
   let(:product_params) do
-        [
-          { name: "トマト", price: 100 },
-          { name: "きゅうり", price: 200 } #不要？
-        ]
-    end
+    [
+      { name: "トマト", price: 100 },
+      { name: "きゅうり", price: 200 }, #不要？
+    ]
+  end
   let(:greengrocer) { Greengrocer.new(product_params) }
   let(:products) { greengrocer.products }
   # ▼単体テスト2 正常系（Greengrocerクラスのインスタンス生成）
   describe ".initialize" do
-    
     context "インスタンスが生成されたとき" do
       it "@productsの数とproduct_paramsの要素の数が同じであること" do
         expect(products.size).to eq 2
@@ -18,7 +17,7 @@ RSpec.describe Greengrocer do
       it "@productsとproduct_paramsそれぞれの1つ目の要素の名前が同じであること" do
         expect(products[0].name).to eq "トマト"
       end
-      
+
       it "@productsとproduct_paramsそれぞれの要素の金額が同じであること" do
         expect(products[0].price).to eq 100
       end
@@ -27,12 +26,11 @@ RSpec.describe Greengrocer do
 
   # ▼単体テスト３ 正常系(register_productメソッド)
   describe ".register_product" do
-
     context "メソッドが実行されたとき" do
       let(:adding_product_params) do
         [
-          {name: "ごぼう", price: 250},
-          {name: "れんこん", price: 350}
+          { name: "ごぼう", price: 250 },
+          { name: "れんこん", price: 350 },
         ]
       end
       it "@productsの要素の数と「product_paramsとadding_product_paramsの要素の数の和」が同じであること" do
@@ -44,7 +42,7 @@ RSpec.describe Greengrocer do
         greengrocer.register_product(adding_product_params)
         expect(products[-1].name).to eq "れんこん"
       end
-      
+
       it "@productsの最後の要素とadding_product_paramsの最後の要素の名前が同じであること" do
         greengrocer.register_product(adding_product_params)
         expect(products[-1].price).to eq 350
@@ -56,40 +54,27 @@ RSpec.describe Greengrocer do
   describe ".disp_products" do
     context "メソッドが実行されたとき" do
       it "出力処理ができていること" do
-        base_id = Product.class_variable_get('@@count')
+        base_id = Product.class_variable_get("@@count")
         hello_msg = "いらっしゃいませ！商品を選んで下さい。"
         product_msg1 = "#{base_id + 1}.トマト(¥100)"
         product_msg2 = "#{base_id + 2}.きゅうり(¥200)"
         msg = "#{hello_msg}\n#{product_msg1}\n#{product_msg2}\n"
-        expect { greengrocer.disp_products }
-          .to output(msg).to_stdout
+        expect { greengrocer.disp_products }.to output(msg).to_stdout
       end
     end
   end
-end
+
 
 # ▼単体テスト6 正常系(ask_quantityメソッド)
-describe ".ask_quantity" do
-  # let(:product_params) do
-  #   [
-  #     { name: "トマト", price: 100 },
-  #     { name: "きゅうり", price: 200 }
-  #   ]
-  # end
-  # let(:greengrocer) { Greengrocer.new(product_params) }
-  # let(:products) { greengrocer.products }
-  # let(:user) { User.new }
-  # let(:correct_input) { "#{products.last.id}\n" }
-  let(:chosen_product) { Product.new({name: "なす", price: 400}) }
-  let(:ask_msg) { "なすですね。何個買いますか？\n" }
-  context "メソッドが実行されたとき" do
-    it "userが選択した商品の名前を含む，期待した表示がされること" do
-      # allow(ARGF).to receive(:gets).and_return correct_input
-      # user.choose_product(products)
-      expect{ greengrocer.ask_quantity(chosen_product)}.to output(ask_msg).to_stdout
+  describe ".ask_quantity" do
+    let(:chosen_product) { Product.new({ name: "なす", price: 400 }) }
+    let(:ask_msg) { "なすですね。何個買いますか？\n" }
+    context "メソッドが実行されたとき" do
+      it "userが選択した商品の名前を含む，期待した表示がされること" do
+        expect { greengrocer.ask_quantity(chosen_product) }.to output(ask_msg).to_stdout
+      end
     end
   end
-end
 
 # # 【単体テスト8 正常系】calculate_chargesメソッドが期待通り動作する
 # #  --> 条件分岐が複数あるので，quantity_of_productが4個のときと5個のときだけでなく，chosen_productも2種類ぐらい確認しておいた方が無難。
@@ -111,7 +96,7 @@ end
 #   context "quantity_of_productが4個，chosen_productがトマトのとき" do
 #     let(:chosen_product){ Product.new({ name: "トマト", price: 100 }) }
 #     let(:quantity_of_product) { 4 }
-    
+
 #     it "正しい合計金額を含む，期待した表示がされること" do
 #       user.instance_variable_set("@chosen_product", chosen_product)
 #       user.instance_variable_set("@quantity_of_product", quantity_of_product)
@@ -122,7 +107,7 @@ end
 #   context "quantity_of_productが4個，chosen_productがきゅうりのとき" do
 #     let(:chosen_product){ Product.new({ name: "きゅうり", price: 200 }) }
 #     let(:quantity_of_product) { 4 }
-    
+
 #     it "正しい合計金額を含む，期待した表示がされること" do
 #       user.instance_variable_set("@chosen_product", chosen_product)
 #       user.instance_variable_set("@quantity_of_product", quantity_of_product)
@@ -133,7 +118,7 @@ end
 #   context "quantity_of_productが5個，chosen_productがトマトのとき" do
 #     let(:chosen_product){ Product.new({ name: "トマト", price: 100 }) }
 #     let(:quantity_of_product) { 5 }
-    
+
 #     it "正しい合計金額を含む，期待した表示がされること" do
 #       user.instance_variable_set("@chosen_product", chosen_product)
 #       user.instance_variable_set("@quantity_of_product", quantity_of_product)
@@ -144,7 +129,7 @@ end
 #   context "quantity_of_productが5個，chosen_productがきゅうりのとき" do
 #     let(:chosen_product){ Product.new({ name: "きゅうり", price: 200 }) }
 #     let(:quantity_of_product) { 5 }
-    
+
 #     it "正しい合計金額を含む，期待した表示がされること" do
 #       user.instance_variable_set("@chosen_product", chosen_product)
 #       user.instance_variable_set("@quantity_of_product", quantity_of_product)
@@ -152,7 +137,7 @@ end
 #     end
 #   end
 # end
-
+end
 # # # 総合テストになっている
 # # describe ".grenngrocer_program" do #テスト名は仮
 # #   let(:product_params) do
@@ -229,7 +214,7 @@ end
 
 # # quantity_of_productが4個のとき,
 # # chosen_productがトマトのとき
-# #  "合計金額は#{total_price.floor}円です。"と出力される 
+# #  "合計金額は#{total_price.floor}円です。"と出力される
 # # chosen_productがきゅうりのとき
 # #  "合計金額は#{total_price.floor}円です。"と出力される
 
