ab863770d594b90f179c8d74e8ac6b98e285aaf2
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-03-05 12:03:09 +0900

    単体テスト3のitの中を修正

--- spec/units/greengrocer_spec.rb
+++ spec/units/greengrocer_spec.rb
@@ -42,7 +42,7 @@ RSpec.describe Greengrocer do
       expect(products[-1].name).to eq "れんこん"
     end
 
-    it "@productsの最後の要素とadding_product_paramsの最後の要素の名前が同じであること" do
+    it "@productsの最後の要素とadding_product_paramsの最後の金額の金額が同じであること" do
       greengrocer.register_product(adding_product_params)
       expect(products[-1].price).to eq 350
     end
