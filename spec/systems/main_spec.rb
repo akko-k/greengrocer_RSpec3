bdd18e76737ba3f5421ec34b3037bb23f3c6764d
kawakami <barcarolle.op60.horowitz@gmail.com>
2021-03-28 09:59:16 +0900

    総合テストをの文言修正/不要な部分を削除

--- spec/systems/main_spec.rb
+++ spec/systems/main_spec.rb
@@ -19,8 +19,8 @@ describe ".grenngrocer_program" do
   let(:user) { User.new }
   let(:thank_msg) { "お買い上げありがとうございました！" }
 
-  shared_examples "正しい合計金額を含む，期待した表示がされること" do
-    it "正しい合計金額を含む，期待した表示がされること" do
+  shared_examples "正しい合計金額を含む，期待する表示がされること" do
+    it do
       # 商品を登録（adding_product_params1 の商品を追加）
       greengrocer1.register_product(adding_product_params1)
       # 商品を表示
@@ -38,7 +38,7 @@ describe ".grenngrocer_program" do
     end
   end
 
-  shared_examples "割引した正しい合計金額を含む，期待した表示がされること" do
+  shared_examples "割引した正しい合計金額を含む，期待する表示がされること" do
     it do
       # 商品を登録（adding_product_params1 の商品を追加）
       greengrocer1.register_product(adding_product_params1)
@@ -61,14 +61,14 @@ describe ".grenngrocer_program" do
     let(:correct_product_id_input) { "#{products.first.id}\n" }
     let(:correct_quantity_input) { "4\n" }
     let(:total_price_msg) { "合計金額は400円です。" }
-    it_behaves_like "正しい合計金額を含む，期待した表示がされること"
+    it_behaves_like "正しい合計金額を含む，期待する表示がされること"
   end
 
   context "最後のidの商品が4個のとき" do
     let(:correct_product_id_input) { "#{products.last.id}\n" }
     let(:correct_quantity_input) { "4\n" }
     let(:total_price_msg) { "合計金額は1400円です。" }
-    it_behaves_like "正しい合計金額を含む，期待した表示がされること"
+    it_behaves_like "正しい合計金額を含む，期待する表示がされること"
   end
 
   context "最初のidの商品が5個のとき" do
@@ -76,7 +76,7 @@ describe ".grenngrocer_program" do
     let(:correct_quantity_input) { "5\n" }
     let(:discount_msg) { "5個以上なので10％割引となります！" }
     let(:discount_total_price_msg) { "合計金額は450円です。" }
-    it_behaves_like "割引した正しい合計金額を含む，期待した表示がされること"
+    it_behaves_like "割引した正しい合計金額を含む，期待する表示がされること"
   end
 
   context "最後のidの商品が5個のとき" do
@@ -84,6 +84,6 @@ describe ".grenngrocer_program" do
     let(:correct_quantity_input) { "5\n" }
     let(:discount_msg) { "5個以上なので10％割引となります！" }
     let(:discount_total_price_msg) { "合計金額は1575円です。" }
-    it_behaves_like "割引した正しい合計金額を含む，期待した表示がされること"
+    it_behaves_like "割引した正しい合計金額を含む，期待する表示がされること"
   end
 end
